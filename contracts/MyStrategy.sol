527ca52262ad9b142e540325e0bf59426b3852a9
Entreprenerd <valeranijr@gmail.com>
2021-09-09 19:43:14 +0200

    revert: tend needs same accessor for some reason

--- contracts/MyStrategy.sol
+++ contracts/MyStrategy.sol
@@ -362,6 +362,7 @@ contract MyStrategy is BaseStrategy {
 
     /// @dev Rebalance, Compound or Pay off debt here
     function tend() external whenNotPaused {
+        _onlyAuthorizedActors();
         revert(); // NOTE: For now tend is replaced by manualRebalance
     }
 
