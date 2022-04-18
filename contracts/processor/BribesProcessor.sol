b79fd6b11a77431e09d56c9d3c073d5be88c4823
Entreprenerd <valeranijr@gmail.com>
2022-04-18 15:18:22 +0200

    feat: ported over code

---
+++ contracts/processor/BribesProcessor.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+contract BribesProcessor is VotiumBribesProcessor {
+  /// Using inheritance to import release from other brownie project
+  /// DO NOT CHANGE THIS
+  /// Chanding this file breaks trust in the code (untested)
+}
